455a85408bcafc87e4cf6f83133bed5129465ddf
Julián Castrillón <juliancastrillon681@gmail.com>
2022-11-29 23:54:23 -0500

    Slider terminado con 5 imágenes. Slider automático cada 5 segundos.

--- scripts/script.js
+++ scripts/script.js
@@ -2,10 +2,55 @@
 
 addEventListener('load',()=>{
     // Declaración de variables; 
+    const sliderContent = document.querySelector('#slider-content'); 
+    let sliderImg = document.querySelectorAll('.sliderImg');
+    let sliderLast = sliderImg[sliderImg.length - 1]; // Obtenemos la última imagen para este caso; 
+    const leftButton = document.querySelector('#button-left'); 
+    const rightButton = document.querySelector('#button-right'); 
 
-    // Inicialización de variables; 
-
+    
     // Instrucciones; 
+    // Sigue estando la primera imágen como primera vista
+    sliderContent.insertAdjacentElement('afterbegin', sliderLast); // Ubicamos la última imágen al comienzo del slider;
 
-    // Funciones; 
+    rightButton.addEventListener('click',()=>{
+        next(); 
+    }); 
+
+    leftButton.addEventListener('click',()=>{
+        prev(); 
+    }); 
+
+    setInterval(()=>{
+        // Para que el recorrido sea automático; 
+        next(); 
+    },5000); 
+
+
+    // Funciones;
+    let next = ()=>{
+        let sliderFirst = document.querySelectorAll('.sliderImg')[0]; //Esta vez nos interesa la primera imágen; 
+        sliderContent.style.marginLeft = "-200%"; 
+        sliderContent.style.transition = "all 0.5s";
+        
+        setTimeout(()=>{
+            sliderContent.style.transition = "none"; 
+            sliderContent.insertAdjacentElement('beforeend', sliderFirst);
+            sliderContent.style.marginLeft = "-100%"; 
+        },500);
+    };
+
+    let prev = ()=>{
+        let sliderImg = document.querySelectorAll('.sliderImg');
+        let sliderLast = sliderImg[sliderImg.length - 1];
+        sliderContent.style.marginLeft = '0%';
+        sliderContent.style.transition = "all 0.5s";
+
+        setTimeout(()=>{
+            sliderContent.style.transition = "none"; 
+            sliderContent.insertAdjacentElement('afterbegin',sliderLast); 
+            sliderContent.style.marginLeft = '-100%';
+
+        },500); 
+    };  
 }); 
